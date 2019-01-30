514956f8497ed425c3370842990d48502d242fc6
roshanbrainitec11 <47177946+roshanbrainitec11@users.noreply.github.com>
2019-01-30 18:27:29 +0530

    first commit
    first

---
+++ server.js
@@ -0,0 +1,31 @@
+// This app serves some static content from a static path and serves a REST API that's
+// defined on the api.yaml swagger 2.0 file
+// Usage:
+// node server -h
+
+var SwaggerBP = require('swagger-boilerplate');
+var Utils = SwaggerBP.Utils;
+var Logger = Utils.MultiLevelLogger;
+var logger = new Logger('HTTP Server App');
+
+var Server = require('swagger-boilerplate').Server;
+
+var server =
+ new Server({
+   apiFile: './api.yml',
+   modulePath: __dirname + '/server/',
+   appName: 'OpenTokRTC Main',
+   staticOptions: {
+     dotfiles: 'ignore',
+     extensions: ['jpg'],
+     index: false,
+     redirect: false,
+     setHeaders: function(res, path) {
+       if (path.indexOf('/images/background.jpg') > -1) {
+         res.set('Cache-Control', 'max-age=31536000')
+       }
+     }
+   }
+ });
+
+server.start();
